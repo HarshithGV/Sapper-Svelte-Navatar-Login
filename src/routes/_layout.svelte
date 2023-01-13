bc5fec9a230cf16d9ee8f49a089133dca461cf7e
Harshith <harshithgv.sangamone@gmail.com>
2023-01-13 11:38:54 +0530

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+
+	
+
+<Nav {segment}/>
+
+<main class="form-signin w-100 m-auto">
+	<slot></slot>
+</main>
